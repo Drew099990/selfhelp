0eab3d9cd8a563013a36b55bbc363343f2d1bbd1
Andrew muyunda <andrewmmuyunda777@gmail.com>
2025-02-15 02:18:24 +0200

    newer

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import SelfHelp from './selfHelp'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import SelfHelp from './selfHelp';
 import HomeSelf from './selfHelp/home';
 import Community from './selfHelp/community';
 import CommunityBody from './selfHelp/communityBody';
 
-const router = createBrowserRouter([{path:"/",element:<SelfHelp/>},
-  {path:"/home",element:<HomeSelf/>},
-  {path:"/community",element:<Community/>},
-  {path:"/c-body",element:<CommunityBody/>}])
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
- <RouterProvider router={router}/>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<SelfHelp />} />
+        <Route path="/home" element={<HomeSelf />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/c-body" element={<CommunityBody />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
-)
+);
